457a45e68b9d561481282df8183a1bc98475b286
mgaertner <mgaertner@82ab269c-af05-4124-b5e1-9e6983fa38f1>
2012-07-18 09:50:10 +0000

    test GetEnumeratorAllChildren

--- trunk/test/testxdb.pas
+++ trunk/test/testxdb.pas
@@ -2,6 +2,7 @@
  Test with:
      ./runtests --format=plain --suite=TTestXDB
      ./runtests --format=plain --suite=TestReadXDB
+     ./runtests --format=plain --suite=TestXDBEnumerators
 }
 unit TestXDB;
 
@@ -19,11 +20,23 @@ type
   { TTestXDB }
 
   TTestXDB = class(TTestCase)
+  private
   published
     procedure TestReadXDB;
+    procedure TestXDBEnumerators;
   end;
 
+function GetXDBNodeName(Node: TXDBNode): string;
+
 implementation
+
+function GetXDBNodeName(Node: TXDBNode): string;
+begin
+  if Node=nil then
+    Result:='nil'
+  else
+    Result:=Node.GetName;
+end;
 
 { TTestXDB }
 
@@ -34,6 +47,32 @@ begin
   Root:=nil;
   try
     ReadXDBTree(Root,SetDirSeparators('xml/MKPosteriorAnalyticsLat.xml'));
+  finally
+    Root.Free;
+  end;
+end;
+
+procedure TTestXDB.TestXDBEnumerators;
+var
+  Root: TXDBRootNode;
+  Node: TXDBNode;
+  LastNode: TXDBNode;
+  ExpectedNode: TXDBNode;
+begin
+  Root:=nil;
+  try
+    ReadXDBTree(Root,SetDirSeparators('xml/enumeration.xml'));
+    LastNode:=nil;
+    for Node in Root.GetEnumeratorAllChildren do begin
+      if Node.GetPrev<>LastNode then begin
+        ExpectedNode:=Node.GetPrev;
+        if ExpectedNode=Root then ExpectedNode:=nil;
+        AssertEquals('GetEnumeratorAllChildren match'
+          +' Expected='+GetXDBNodeName(ExpectedNode)+' Last='+GetXDBNodeName(LastNode),
+          true,ExpectedNode=LastNode);
+      end;
+      LastNode:=Node;
+    end;
   finally
     Root.Free;
   end;
